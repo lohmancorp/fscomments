3a81c82405ccc6a881d644dd0ed13d399b1bfa9e
lohmancorp <45764849+lohmancorp@users.noreply.github.com>
2023-12-04 14:09:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 # setup.py is a script to setup comments.py
 #
 # Author: Taylor Giddens - taylor.giddens@ingrammicro.com
-# Version: 1.01
+# Version: 1.04
 ################################################################################
 
 import os
+import requests
 import subprocess
 import sys
+from dotenv import load_dotenv
 
 def check_python():
     if sys.version_info < (3, 6):
@@ -20,6 +22,15 @@ def install_packages():
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
     print("Required packages installed successfully.")
 
+def download_file_from_github(file_url, destination):
+    response = requests.get(file_url)
+    if response.status_code == 200:
+        with open(destination, 'wb') as file:
+            file.write(response.content)
+        print(f"Downloaded {destination}")
+    else:
+        print(f"Failed to download {destination}")
+
 def setup_env_file():
     env_variables = {
         'API_KEY': '',
@@ -28,25 +39,26 @@ def setup_env_file():
         'LOG_DIRECTORY': ''
     }
 
-    print("\nSetting up environment variables...")
     if not os.path.isfile('.env'):
         with open('.env', 'w') as file:
             for key in env_variables:
                 value = input(f"Enter value for {key}: ")
-                file.write(f"{key}={value}\n")
+                file.write(f"{key}=\"{value}\"\n")  # Enclosing the value in quotes
                 print(f"{key} set successfully.")
     else:
         print(".env file already exists.")
 
 def create_directories():
-    directories = ['./versions', './documentation', './logs']
-    print("\nCreating necessary directories...")
+    # Load the environment variables from .env file
+    load_dotenv()
+    
+    log_directory = os.getenv('LOG_DIRECTORY', './logs')
+    directories = ['./documentation', log_directory]
+
     for directory in directories:
         if not os.path.exists(directory):
             os.makedirs(directory)
             print(f"Directory {directory} created.")
-        else:
-            print(f"Directory {directory} already exists.")
     print("All necessary directories created successfully.")
 
 def main():
@@ -55,8 +67,16 @@ def main():
     install_packages()
     setup_env_file()
     create_directories()
+
+    # URLs for the raw content on GitHub
+    comments_py_url = "https://raw.githubusercontent.com/lohmancorp/fscomments/main/comments.py"
+    documentation_url = "https://raw.githubusercontent.com/lohmancorp/fscomments/main/comments_script.pdf"
+
+    # Downloading the files
+    download_file_from_github(comments_py_url, "comments.py")
+    download_file_from_github(documentation_url, "./documentation/comments_script.pdf")
+
     print("\nSetup completed successfully.")
 
 if __name__ == "__main__":
     main()
-
